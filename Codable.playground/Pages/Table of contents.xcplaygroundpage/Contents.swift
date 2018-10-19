6622ced644a2dbe5669313cd8fdf7f6effe0f79a
Bin Chen <bin@skyrock.com>
2018-10-19 15:59:39 +0200

    first page

---
+++ Codable.playground/Pages/Table of contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+/*: Codable
+
+# Table of contents
+
+* [老张]
+* [Simple1]
+* [Simple1]
+
+ */
+
+//: [Simple1](@next)
---
+++ Codable.playground/Pages/老张.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let jsonStr = """
+{
+    "id" : "10001",
+    "name" : "老张",
+    "age" : 23,
+    "face" : "😊"
+}
+"""
+
+struct User: Codable {
+    let id: String
+    let name: String
+    let age: UInt
+    let face: String
+}
+
+let jsonDecoder = JSONDecoder()
+let data = jsonStr.data(using: .utf8)
+let user = try jsonDecoder.decode(User.self, from: data!)
+
+print(user)
+
+//: [Next](@next)
